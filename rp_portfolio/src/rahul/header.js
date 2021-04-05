6b454bccaa1ac7e34e174d9e436ff04a26863936
Rahulsingh053 <80709254+Rahulsingh053@users.noreply.github.com>
2021-04-06 00:51:06 +0530

    creating my components

---
+++ rp_portfolio/src/rahul/header.js
@@ -0,0 +1,14 @@
+import "./header.css"
+export default function Header(){
+    return(
+    <div id="header">
+
+        <div id="port">
+        Social Connect
+        </div>
+        <div id="port2">
+        Education
+    </div>
+    </div>
+    )
+}
--- rp_portfolio/src/rahul/index.js
+++ rp_portfolio/src/rahul/index.js
@@ -1,10 +1,15 @@
-
+import Header from "./header";
+import Name from "./name";
+import Skills from "./skills";
 
 export default function Main () {
 
     return (
         <div>
             Hi Rahul
+            <Header></Header>
+            <Name></Name>
+            <Skills></Skills>
         </div>
     )
 }
