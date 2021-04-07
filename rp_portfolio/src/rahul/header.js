104599a66c36ae3e40c8e5fed5325f4ec1631cab
Rahulsingh053 <80709254+Rahulsingh053@users.noreply.github.com>
2021-04-07 23:14:22 +0530

    third step

--- rp_portfolio/src/rahul/header.js
+++ rp_portfolio/src/rahul/header.js
@@ -1,10 +1,13 @@
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
 import "./header.css"
 export default function Header(){
     return(
     <div id="header">
 
         <div id="port">
-        Social Connect
+        <a href="mailto:someone@example.com"><MdEmail className="email"></MdEmail></a>
+        <a href="https://www.linkedin.com/in/rahul-pratap-singh-353753202/"><FaLinkedin class="linkedin"></FaLinkedin></a>
         </div>
         <div id="port2">
         Education
