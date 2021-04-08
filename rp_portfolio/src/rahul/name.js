28a0eb4692e62b4639328cf099af11e1c1fe8426
Rahulsingh053 <80709254+Rahulsingh053@users.noreply.github.com>
2021-04-08 22:46:30 +0530

    5 ahead

--- rp_portfolio/src/rahul/name.js
+++ rp_portfolio/src/rahul/name.js
@@ -1,13 +1,13 @@
 import "./name.css"
-import profileimage from "./IMG_20180916_210101.jpg"
+import profileimage from "./WhatsApp Image 2021-04-08 at 10.38.02 PM.jpeg"
 export default function Name(){
     return(<div>
 
-        <div id="name">
-            Rahul Pratap Singh
-        </div>
         <div id="name1">
             <img src={profileimage}></img>
+        </div>
+        <div id="name">
+            Rahul Pratap Singh
         </div>
     </div>
     )
