acaef413b386f6933c1c665a98035287df49ea4c
Rahulsingh053 <80709254+Rahulsingh053@users.noreply.github.com>
2021-04-09 00:27:29 +0530

    6 ahead

--- rp_portfolio/src/rahul/name.js
+++ rp_portfolio/src/rahul/name.js
@@ -3,10 +3,10 @@ import profileimage from "./WhatsApp Image 2021-04-08 at 10.38.02 PM.jpeg"
 export default function Name(){
     return(<div>
 
-        <div id="name1">
+        <div id="rname1">
             <img src={profileimage}></img>
         </div>
-        <div id="name">
+        <div id="rname">
             Rahul Pratap Singh
         </div>
     </div>
--- rp_portfolio/src/rahul/skills.js
+++ rp_portfolio/src/rahul/skills.js
@@ -1,8 +1,45 @@
 import "./skills.css"
-export default function Skills(){
-    return(
-    <div id="skills">
-        <span>HTML &nbsp;&nbsp;&nbsp;&nbsp; CSS &nbsp;&nbsp;&nbsp;&nbsp; PYTHON &nbsp;&nbsp;&nbsp;&nbsp; JAVASCRIPT &nbsp;&nbsp;&nbsp;&nbsp; ML &nbsp;&nbsp;&nbsp;&nbsp; DATA_SCIENCE</span>
+export default function Skills() {
+    const skilllist=[
+        {
+            name: "HTML",
+            rating: 50
+        },
+        {
+            name: "PYTHON",
+            rating: 50
+        },
+        {
+            name: "CSS",
+            rating: 50
+        },
+        {
+            name: "MACHINE LEARNING",
+            rating: 50
+        },
+        {
+            name: "DATA SCIENCE",
+            rating: 50
+        }
+
+    ]
+
+
+    return (<div>
+            <h2>Technical Skills</h2>
+        <div id="skills">
+            {skilllist.map(s=> <div class="dirpl"><h2>{s.name}</h2>  <div class="rslidercontainer">
+                <div class="rslider">
+
+                </div>
+            </div></div>)}
+            
+           
+        </div>
+        
+           
     </div>
+
+    
     )
 }
