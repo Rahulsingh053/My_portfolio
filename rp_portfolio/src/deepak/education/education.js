1eaf95f599e81f4e8532952178e57d29d99469b3
deepak061 <80782673+deepak061@users.noreply.github.com>
2021-04-16 14:37:44 +0530

    changes

---
+++ rp_portfolio/src/deepak/education/education.js
@@ -0,0 +1,16 @@
+import "./education.css";
+export default function Education() {
+  return (
+    <div>
+      <div id="education">
+        <h2>Education</h2>
+      </div>
+
+      <div id="education">
+        <li>S.S.C = 64%</li>
+        <li>H.S.C = 55%</li>
+        <li>Bachelor of Engineering(Computers) = 7.27 CGPA</li>
+      </div>
+    </div>
+  );
+}
--- rp_portfolio/src/deepak/index.js
+++ rp_portfolio/src/deepak/index.js
@@ -1,14 +1,18 @@
 import Sidebar from "./sidebar/sidebar";
-import About from "./about";
+import About from "./about/about";
+import Education from "./education/education";
 import "./index.css";
 export default function Main() {
   return (
     <div class="maincss">
       <Sidebar></Sidebar>
       <div className="box">
-<About></About>
+        <About></About>
+        <Education></Education>
+
       </div>
-      <div className="box"></div>
+      <div className="box">
+      </div>
       <div className="box"></div>
     </div>
   );
